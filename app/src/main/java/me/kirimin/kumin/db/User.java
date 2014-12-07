af197614c6512c53abb465d87176ada5e77b22cf
kirimin <cc@kirimin.me>
2014-12-07 15:15:33 +0900

    タブスペースのファイルにフォーマッターかけた。

--- app/src/main/java/me/kirimin/kumin/db/User.java
+++ app/src/main/java/me/kirimin/kumin/db/User.java
@@ -5,37 +5,46 @@ import java.util.List;
 
 public class User {
 
-	private String id;
-	private String sName;
-	private String token;
-	private String secret;
-	
-	public User(String id, String screenName, String token, String secret) {
-		this.id = id;
-		this.sName = screenName;
-		this.token = token;
-		this.secret = secret;
-	}
+    private String id;
+    private String sName;
+    private String token;
+    private String secret;
 
-	public String getId()     { return id; }
-	
-	public String getSName()  { return sName; }
-	
-	public String getToken()  { return token; }
-	
-	public String getSecret() { return secret; }
-	
-	/***
-	 * Userのリストから対応したスクリーンネームの配列を取得する
-	 * @param users Userのリスト
-	 * @return Userのリストに対応したスクリーンネームの配列
-	 */
-	public static List<String> getUserNames(List<User> users){
-		List<String> userNames = new ArrayList<String>();
-		for(User user : users){
-			userNames.add(user.getSName());
-		}
-		
-		return userNames;
-	}
+    public User(String id, String screenName, String token, String secret) {
+        this.id = id;
+        this.sName = screenName;
+        this.token = token;
+        this.secret = secret;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSName() {
+        return sName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    /**
+     * Userのリストから対応したスクリーンネームの配列を取得する
+     *
+     * @param users Userのリスト
+     * @return Userのリストに対応したスクリーンネームの配列
+     */
+    public static List<String> getUserNames(List<User> users) {
+        List<String> userNames = new ArrayList<String>();
+        for (User user : users) {
+            userNames.add(user.getSName());
+        }
+
+        return userNames;
+    }
 }
